124b776596b6861807ef62b66376def1ed8e8e39
haniful360 <haniful360@gmail.com>
2023-05-19 20:40:31 +0600

    my toys item delete

--- index.js
+++ index.js
@@ -69,6 +69,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/toys/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await toyCollection.deleteOne(query);
+      res.send(result)
+      console.log('delete',id);
+    })
 
 
 
