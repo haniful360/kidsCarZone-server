3eb87b68e3d74e08884d57b9847f88cad8f062e9
haniful360 <haniful360@gmail.com>
2023-05-19 12:54:00 +0600

    vercel config file added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -30,11 +30,30 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const toyCollection = client.db('kidsCarZone').collection("addToys")
+    // const categoryCollection = client.db('kidsCarZone').collection('category')
+    // const subCategoryCollection = client.db('kidsCarZone').collection('subCategory')
 
-    app.post('/toys', async(req, res) =>{
+    app.get('/toys/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await toyCollection.findOne(query)
+      res.send(result);
+    })
+
+    app.get('/toys', async (req, res) => {
+      console.log(req.query);
+      let query = {};
+      if (req.query?.email) {
+        query={ email: req.query.email }
+      }
+      const result = await toyCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post('/toys', async (req, res) => {
       const body = req.body;
       const result = await toyCollection.insertOne(body)
       res.send(result);
